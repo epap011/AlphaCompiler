aa1cc36628129896230c5c0d6dd3fcfdb8016423
panoklis <csd3665@csd.uoc.gr>
2023-02-20 21:07:31 +0200

    feat: enum & alpha_token_t modification

--- phase1_lex/token.c
+++ phase1_lex/token.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "tokens.h"
 
-void insert_token(alpha_token_t *tokenList, int numline, int numToken, char *content, enum type tType){
+void insert_token(alpha_token_t *tokenList, int numline, int numToken, char *content, enum type tType, enum subtype sType){
     alpha_token_t *tail = tokenList->tail;
     alpha_token_t *head = tokenList->head;
     
@@ -10,6 +10,7 @@ void insert_token(alpha_token_t *tokenList, int numline, int numToken, char *con
         tail->next->numline=numline;
         tail->next->numToken=numToken;
         tail->next->tType=tType;
+        tail->next->sType=sType;
         tail->next->content=malloc(sizeof(char)*strlen(content) + 1);
         memcpy(tail->next->content, content, strlen(content) + 1);
         tail=tail->next;
@@ -20,6 +21,7 @@ void insert_token(alpha_token_t *tokenList, int numline, int numToken, char *con
         head->numline=numline;
         head->numToken=numToken;
         head->tType=tType;
+        head->sType=sType;
         head->content=malloc(sizeof(char)*strlen(content) + 1);
         memcpy(head->content, content, strlen(content) + 1);
         tail->next=NULL;
