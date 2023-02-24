100aab8d49a484c684a3b4e748e94a26a480dad5
Efthymis Papageorgiou <papageorgiou.efthymis@protonmail.com>
2023-02-24 11:53:21 +0200

    fix(by-hand): final diagram states has been removed

--- phase1_lex/byhand_lex.c
+++ phase1_lex/byhand_lex.c
@@ -5,11 +5,11 @@
 #include "tokens.h"
 
 #define MAX_LEXEME  1024
-#define MAX_STATE   6
+#define MAX_STATE   11
 #define TOKEN_SHIFT (MAX_STATE+1)
 #define TOKEN(t)    TOKEN_SHIFT+(t)
 #define STATE(s)    s
-#define ISTOKEN(s)  ((s) >= TOKEN_STATE)
+#define ISTOKEN(s)  ((s) >= MAX_STATE)
 
 FILE* inputFile   = (FILE*)0;
 
@@ -24,7 +24,7 @@ char lookAhead = '\0';
 
 int sf0 (char c); int sf1 (char c); int sf2 (char c); int sf3 (char c); int sf4 (char c); 
 int sf5 (char c); int sf6 (char c); int sf7 (char c); int sf8 (char c); int sf9 (char c);
-int sf10(char c); int sf11(char c); int sf12(char c);
+int sf10(char c); int sf11(char c);
 
 void     resetLexeme(void); void checkLine(char c);
 char     getNextChar(void); void retrack(char c);
@@ -33,7 +33,7 @@ char*    getLexeme(void);   int  isAlpha(char c);
 int      isSpace(char c);   int  isPancuation(char c);
 int      isDigit(char c); 
 
-int (*state_funcs[MAX_STATE+1])(char) = {&sf0, &sf1, &sf2, &sf3, &sf4, &sf5, &sf6, &sf7, &sf8, &sf9, &sf10, &sf11, &sf12};
+int (*state_funcs[MAX_STATE+1])(char) = {&sf0, &sf1, &sf2, &sf3, &sf4, &sf5, &sf6, &sf7, &sf8, &sf9, &sf10, &sf11};
 
 int main(int argc, char** argv) {
     if(argc != 2) {
@@ -60,11 +60,18 @@ int sf0 (char c) {
     if(c == '>')   return STATE(4);
     if(c == '+')   return STATE(5);
     if(c == '-')   return STATE(6);
-    if(c == '*')   return STATE(7);
-    if(c == '/')   return STATE(8);
-    if(c == '%')   return STATE(9);
-    if(isAlpha(c) || isDigit(c)) return STATE(10);
-    if(isPancuation(c))          return STATE(11);
+    if(c == '*') {
+        extendLexeme(c);
+        return TOKEN(MUL);
+    }
+    if(c == '%')   {
+        extendLexeme(c);
+        return TOKEN(MOD);
+    }
+    if(c == '/')   return STATE(7);
+    if(isAlpha(c)) return STATE(8);
+    if(isDigit(c)) return STATE(9);
+    if(isPancuation(c)) return STATE(10);
     if(isSpace(c)) {
         checkLine(c);
         return STATE(12);
@@ -132,26 +139,14 @@ int sf6(char c) {
     return TOKEN(MINUS);
 }
 
-/* read * */
-int sf7(char c) {
-    extendLexeme(c);
-    return TOKEN(MUL);
-}
-
 /* read / */
+int sf7(char c) {
+    /* signle line comment OR block comment OR division */
+}
+
+/* read letter | digit */
 int sf8(char c) {
-    /* signle line comment OR block comment or division */
-}
-
-/* read % */
-int sf9(char c) {
-    extendLexeme(c);
-    return TOKEN(MOD);
-}
-
-/* read identifier or number */
-int sf10(char c) {
-    if(isalpha(c) || isdigit(c)) {
+    if(isAlpha(c) || isDigit(c)) {
         extendLexeme(c);
         return STATE(10);
     }
@@ -159,7 +154,18 @@ int sf10(char c) {
     return TOKEN(IDENT);
 }
 
-int sf11(char c) {
+/* read Digit */
+int sf9(char c) {
+    if(isDigit(c)) {
+        extendLexeme(c);
+        return STATE(11);
+    }
+    retrack(c);
+    return TOKEN(NUMBER);
+}
+
+/* read punctuation */
+int sf10(char c) {
     if(isPancuation(c)) {
         extendLexeme(c);
         return STATE(11);
@@ -169,7 +175,7 @@ int sf11(char c) {
 }
 
 /* read whitespace */
-int sf12(char c) {
+int sf11(char c) {
     if(isspace(c)) {
         checkLine(c);
         return STATE(11);
@@ -178,19 +184,22 @@ int sf12(char c) {
     return STATE(0);
 }
 
+// sum = sum + 5;
 unsigned gettoken2() {
     state = 0;
-    reserLexeme();
+    resetLexeme();
     while(1) {
         if(feof(inputFile)) return END_OF_FILE;
 
-        char c  = getNextChar();
-        
-        state = (*state_funcs[state])(c);
+        char c = getNextChar();
+        state  = (*state_funcs[state])(c);
 
-        if(state == -1)    return NOSTYPE;           else
-        if(istoken(state)) return state-TOKEN_SHIFT; else
-        extendLexeme(c);
+        if(state == -1)         return NOSTYPE;          
+        else if(ISTOKEN(state)) {
+            doAction();
+            return state-TOKEN_SHIFT;
+        } 
+        else extendLexeme(c);
     }
 }
 
@@ -211,16 +220,6 @@ char* getLexeme(void) {
     return lexeme;
 }
 
-char getNextChar(FILE *inputFile) {
-    if (useLookAhead) {
-        useLookAhead = 0;
-        return lookAhead;
-    } else {
-        assert(!feof(inputFile));
-        return fgetc(inputFile);
-    }
-}
-
 void retrack(char c) {
     assert(!useLookAhead);
     useLookAhead = 1;
@@ -237,17 +236,30 @@ void extendLexeme(char c) {
 }
 
 int isAlpha(char c) {
-    return 1;
+    if('c' >= 65 && 'c' <= 90 || 'c' >= 97 && 'c' <= 122) return STRING;
 }
 
 int isDigit(char c) {
-    return 1;
+    if('c' >= 48 && 'c' <= 57) return NUMBER;
 }
 
 int isSpace(char c) {
-    return 1;
+    if('c' == 32 || 'c' == 9 || 'c' == 10 || 'c' == 13) return 1;
 }
 
 int isPunctuation(char c) {
-    return 1;
+    if('c' == 33 || 'c' == 34 || 'c' == 35 || 'c' == 36 || 
+       'c' == 37 || 'c' == 38 || 'c' == 39 || 'c' == 40 || 
+       'c' == 41 || 'c' == 42 || 'c' == 43 || 'c' == 44 || 
+       'c' == 45 || 'c' == 46 || 'c' == 47 || 'c' == 58 || 
+       'c' == 59 || 'c' == 60 || 'c' == 61 || 'c' == 62 || 
+       'c' == 63 || 'c' == 64 || 'c' == 91 || 'c' == 92 ||
+       'c' == 93 || 'c' == 94 || 'c' == 95 || 'c' == 96 || 
+       'c' == 123 || 'c' == 124 || 'c' == 125 || 'c' == 126) 
+       
+       return PUNCTUATION;
+}
+
+int doAction() {
+
 }
