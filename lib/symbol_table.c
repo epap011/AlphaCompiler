d81059cc057de38d4f0000c2a7158e17f7c5d6c0
Efthymis Papageorgiou <papageorgiou.efthymis@protonmail.com>
2023-03-28 01:05:53 +0300

    fix: symbol table lookup and hide functions

--- lib/symbol_table.c
+++ lib/symbol_table.c
@@ -35,37 +35,34 @@ void symbol_table_destroy(SymbolTable* symbol_table) {
 }
 
 void symbol_table_insert(SymbolTable* symbol_table, Symbol* symbol) {
-    unsigned int hash = hash_function(symbol->value.var_var->name, symbol_table->size);
+    unsigned int index = hash_function(symbol->name, symbol_table->size);
 
-    if(symbol_table->buckets[hash] == NULL) {
-        symbol_table->buckets[hash] = (SymbolTableBucket*)malloc(sizeof(SymbolTableBucket));
-        symbol_table->buckets[hash]->symbol_list = create_linked_list();
+    if( symbol_table->buckets[index] == NULL) {
+        symbol_table->buckets[index] = (SymbolTableBucket*)malloc(sizeof(SymbolTableBucket));
+        symbol_table->buckets[index]->symbol_list = create_linked_list();
     }
 
+    unsigned int scope = symbol->scope;
     if(symbol_table_get_first_symbol_of_scope(symbol_table, scope) == NULL) {
-        symbol_table->first_symbol_scopes[symbol->value.var_var->scope] = symbol;
+        symbol_table->first_symbol_scopes[scope] = symbol;
     }
-
     update_last_symbol_of_scope(symbol_table, symbol);
 
-
-    insert_at_the_end_to_linked_list(symbol_table->buckets[hash]->symbol_list, symbol);
+    insert_at_the_end_to_linked_list(symbol_table->buckets[index]->symbol_list, symbol);
 }
 
-SymbolTableBucket* symbol_table_lookup(SymbolTable* symbol_table, const char* symbol) {
-    unsigned int hash = hash_function(symbol, symbol_table->size);
+SymbolTableBucket* symbol_table_lookup(SymbolTable* symbol_table, const char* symbol, unsigned int scope) {
+    Symbol** first_symbol_of_scope = symbol_table_get_first_symbol_of_scope(symbol_table, scope);
 
-    if(symbol_table->buckets[hash] == NULL) {
-        return NULL;
-    }
+    if(first_symbol_of_scope != NULL) {
+        Symbol* current_symbol = *first_symbol_of_scope;
 
-    linked_list* list = symbol_table->buckets[hash]->symbol_list;
+        while(current_symbol != NULL) {
+            if(current_symbol->is_active && strcmp(current_symbol->name, symbol) == 0) {
+                return current_symbol;
+            }
 
-    for(int i = 0; i < list->size; i++) {
-        Symbol* current_symbol = (Symbol*)get_from_linked_list(list, i);
-
-        if(strcmp(current_symbol->value.var_var->name, symbol) == 0) {
-            return symbol_table->buckets[hash];
+            current_symbol = current_symbol->next_symbol_of_same_scope;
         }
     }
 
@@ -73,17 +70,14 @@ SymbolTableBucket* symbol_table_lookup(SymbolTable* symbol_table, const char* sy
 }
 
 void symbol_table_hide(SymbolTable* symbol_table, unsigned int scope) {
-    for(int i = 0; i < symbol_table->size; i++) {
-        if(symbol_table->buckets[i] != NULL) {
-            linked_list* list = symbol_table->buckets[i]->symbol_list;
+    Symbol** first_symbol_of_scope = symbol_table_get_first_symbol_of_scope(symbol_table, scope);
 
-            for(int j = 0; j < list->size; j++) {
-                Symbol* current_symbol = (Symbol*)get_from_linked_list(list, j);
+    if(first_symbol_of_scope != NULL) {
+        Symbol* current_symbol = *first_symbol_of_scope;
 
-                if(current_symbol->value.var_var->scope == scope) {
-                    current_symbol->is_active = 0;
-                }
-            }
+        while(current_symbol != NULL) {
+            current_symbol->is_active = 0;
+            current_symbol = current_symbol->next_symbol_of_same_scope;
         }
     }
 }
@@ -92,40 +86,24 @@ Symbol* symbol_create(const char* name, unsigned int scope, unsigned int line, i
     Symbol* symbol = (Symbol*)malloc(sizeof(Symbol));
     
     symbol->is_active   = 1;
-    symbol->symbol_type = symbol_type;
     symbol->is_variable = is_variable;
+    symbol->name  = name;
+    symbol->scope = scope;
+    symbol->line  = line;
+    symbol->symbol_type = symbol_type;
     symbol->next_symbol_of_same_scope = NULL;
-
-    if(is_variable) {
-        symbol->value.var_var = (Variable*)malloc(sizeof(Variable));
-        symbol->value.var_var->name  = name;
-        symbol->value.var_var->scope = scope;
-        symbol->value.var_var->line  = line;
-    }
-    else { //is function
-        symbol->value.func_var = (Function*)malloc(sizeof(Function));
-        symbol->value.func_var->name  = name;
-        symbol->value.func_var->scope = scope;
-        symbol->value.func_var->line  = line;
-        symbol->value.func_var->args  = NULL;
-    }
 
     return symbol;
 }
 
 void update_last_symbol_of_scope(SymbolTable* symbol_table, Symbol* symbol) {
-    if(symbol->is_variable) {
-        symbol_table->last_symbol_scopes[symbol->value.var_var->scope] = symbol;
-    }
-    else {
-        symbol_table->last_symbol_scopes[symbol->value.var_var->scope] = symbol;
-    }
+    symbol_table->last_symbol_scopes[symbol->scope] = symbol;
 }
 
 Symbol* symbol_table_get_last_symbol_of_scope(SymbolTable* symbol_table, unsigned int scope) {
-    return (Symbol*)hash_table_get_by_index(symbol_table->last_symbol_scopes, scope);
+    return symbol_table->last_symbol_scopes[scope];
 }
 
 Symbol* symbol_table_get_first_symbol_of_scope(SymbolTable* symbol_table, unsigned int scope) {
-    return (Symbol*)hash_table_get_by_index(symbol_table->first_symbol_scopes, scope);
+    return symbol_table->first_symbol_scopes[scope];
 }
--- lib/symbol_table.h
+++ lib/symbol_table.h
@@ -2,33 +2,16 @@
 #define SYMBOL_TABLE_H
 
 #include "linked_list.h"
-#include "hash_table.h"
-
-typedef struct Variable {
-    const char*  name;
-    unsigned int scope;
-    unsigned int line;
-} Variable;
-
-typedef struct Function {
-    const char*  name;
-    unsigned int scope;
-    unsigned int line;
-    linked_list* args;
-} Function;
 
 enum SymbolType { GLOBAL, LOCAL, FORMAL, USERFUNC, LIBFUNC };
 
 typedef struct Symbol {
     int is_active;
     int is_variable;
-    union {
-        Variable *var_var;
-        Function *func_var;
-    } value;
-
+    const char*  name;
+    unsigned int scope;
+    unsigned int line;
     enum SymbolType symbol_type;
-
     Symbol* next_symbol_of_same_scope;
 } Symbol;
 
@@ -46,9 +29,10 @@ typedef struct SymbolTable {
 SymbolTable*       symbol_table_create ();
 void               symbol_table_destroy(SymbolTable* symbol_table);
 void               symbol_table_insert (SymbolTable* symbol_table, Symbol*      symbol);
-SymbolTableBucket* symbol_table_lookup (SymbolTable* symbol_table, const char*  id);
+SymbolTableBucket* symbol_table_lookup (SymbolTable* symbol_table, const char* symbol, unsigned int scope);
 void               symbol_table_hide   (SymbolTable* symbol_table, unsigned int scope );
 
 Symbol* symbol_create(const char* name, unsigned int scope, unsigned int line, int symbol_type, int is_variable);
 void update_last_symbol_of_scope(SymbolTable* symbol_table, Symbol* symbol);
+
 #endif /* SYMBOL_TABLE_H */
