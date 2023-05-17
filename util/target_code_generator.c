566f0e7a8363e28bc2609bb8ad3dfb776bda4166
Manos <csd4281@csd.uoc.gr>
2023-05-17 15:45:09 +0300

    feat: vmarg structure / initial functions

---
+++ util/target_code_generator.c
@@ -0,0 +1,93 @@
+
+#include "target_code_generator.h"
+
+void make_operand(expr* e, vmarg* arg){
+
+    switch (e->type)
+    {
+    
+    case var_e:
+    case tableitem_e:
+    case arithexpr_e:
+    case boolexpr_e:
+    case newtable_e: {
+        assert(e->sym);
+        arg->val = e->sym->offset;
+        switch (e->sym->space)
+        {
+            case programvar: arg->type = global_a; break;
+            case functionlocal: arg->type = local_a; break;
+            case formalarg: arg->type = formal_a; break;
+            default: assert(0);
+        }
+    } 
+        break;
+
+    case constbool_e: {
+        arg->val = e->boolConst;
+        arg->type = bool_a;
+    }
+        break;
+    
+    case conststring_e: {
+        arg->val = consts_newstring(e->strConst); // make this function
+        arg->type = string_a;
+    }
+        break;
+    
+    case constnum_e: {
+        arg->val = consts_newnumber(e->numConst); // make this function
+        arg->type = number_a;
+    }
+        break;
+
+    case nil_e: 
+        arg->type = nil_a; break;
+
+    case programfunc_e: {
+        arg->type = userfunc_a;
+        arg->val = userfuncs_newfunc(e->sym); // make this function
+    }
+        break;
+
+    case libraryfunc_e: {
+        arg->type = libfunc_a;
+        arg->val = libfuncs_newused(e->sym->name); // make this function
+    }
+        break;
+    
+    default: assert(0);
+    }
+}
+
+void make_numberoperand(vmarg* arg, double val){
+    arg->val = consts_newnumber(val);
+    arg->type = number_a;
+}   
+
+void make_booloperand(vmarg* arg, unsigned val){
+    arg->val = val;
+    arg->type = bool_a;
+}
+
+void make_retvaloperand(vmarg* arg){
+    arg->type = retval_a;
+}
+
+
+unsigned conts_newstring(char* s){
+
+    return 0;
+}
+unsigned conts_newnumber(double n){
+    
+    return 0;
+}
+unsigned libfuncs_newused(char* s){
+
+    return 0;
+}
+unsigned userfuncs_newfunc(Symbol* sym){
+
+    return 0;
+}
--- util/target_code_generator.h
+++ util/target_code_generator.h
@@ -1,18 +1,45 @@
 #ifndef INSTRUCTION_H
 #define INSTRUCTION_H
 
+#include "expression.h"
+
+enum vmarg_t{
+
+    global_a,
+    local_a,
+    formal_a,
+    number_a,
+    string_a,
+    bool_a,
+    nil_a,
+    userfunc_a,
+    libfunc_a,
+    retval_a
+};
+
 typedef struct vmarg {
     vmarg_t type;
     unsigned val;
-} vmarg_t;
+} vmarg;
 
 typedef struct instuction {
     //vmopcode opcode;
-    vmarg_t result;
-    vmarg_t arg1;
-    vmarg_t arg2;
+    vmarg result;
+    vmarg arg1;
+    vmarg arg2;
     unsigned int srcLine;
-} instruction_t;
+} instruction;
+
+void make_operand(expr* e, vmarg* arg);
+void make_numberoperand(vmarg* arg, double val);  //helper for const
+void make_booloperand(vmarg* arg, unsigned val);  //helper for const
+void make_retvaloperand(vmarg* arg);              //helper for return values
+
+
+unsigned conts_newstring(char* s);
+unsigned conts_newnumber(double n);
+unsigned libfuncs_newused(char* s);
+unsigned userfuncs_newfunc(Symbol* sym);
 
 
 #endif /* INSTRUCTION_H */
