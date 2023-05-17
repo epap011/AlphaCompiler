04e31d6f9e406b10062cdaa61f91c21cba923cc4
Efthymis Papageorgiou <papageorgiou.efthymis@protonmail.com>
2023-05-17 14:34:35 +0300

    feat: instruction & vmarg structs

---
+++ src/instruction.h
@@ -0,0 +1,12 @@
+typedef struct vmarg {
+    vmarg_t type;
+    unsigned val;
+} vmarg_t;
+
+typedef struct instuction {
+    //vmopcode opcode;
+    vmarg_t result;
+    vmarg_t arg1;
+    vmarg_t arg2;
+    unsigned int srcLine;
+} instruction_t;
