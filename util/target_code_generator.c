b1963a52a580bd564749e75e77d82f98e27a070e
Efthymis Papageorgiou <papageorgiou.efthymis@protonmail.com>
2023-05-18 11:38:29 +0300

    feat: generate MUL instruction

--- util/target_code_generator.c
+++ util/target_code_generator.c
@@ -152,7 +152,21 @@ void generate_SUB   (quad* q) {
     emit_instruction(i);
 }
 
-void generate_MUL   (quad* q) {}
+void generate_MUL   (quad* q) {
+    instruction* i = (instruction*) malloc(sizeof(instruction));
+    i->opcode  = mul_vm;
+    i->result  = (vmarg*) malloc(sizeof(vmarg));
+    i->arg1    = (vmarg*) malloc(sizeof(vmarg));
+    i->arg2    = (vmarg*) malloc(sizeof(vmarg));
+    i->srcLine = q->line;
+
+    make_operand(q->result, i->result);
+    make_operand(q->arg1  , i->arg1);
+    make_operand(q->arg2  , i->arg2); 
+
+    emit_instruction(i);
+}
+
 void generate_DIV   (quad* q) {}
 void generate_MOD   (quad* q) {}
 void generate_UMINUS(quad* q) {}
