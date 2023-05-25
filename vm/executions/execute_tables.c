639ba23b10454e852ab70874493a83ccaea999c0
Efthymis Papageorgiou <papageorgiou.efthymis@protonmail.com>
2023-05-25 19:23:17 +0300

    feat: execute table creation

--- vm/executions/execute_tables.c
+++ vm/executions/execute_tables.c
@@ -1,8 +1,26 @@
 #include "execute.h"
 #include <stdio.h>
+#include <assert.h>
+
+extern avm_memcell ax, bx, cx;
+extern avm_memcell retval;
+extern unsigned top, topsp;
+extern avm_memcell stack[AVM_STACKSIZE];
+extern unsigned char executionFinished;
+
+extern unsigned currLine;
 
 void execute_newtable(instruction* instr){
     printf("execute_newtable\n");
+
+    avm_memcell* lv = avm_translate_operand(instr->result, (avm_memcell*) 0);
+    assert(lv && ((&stack[AVM_STACKSIZE - 1] >= lv && lv > &stack[top]) || lv == &retval));
+
+    avm_memcell_clear(lv);
+
+    lv->type = table_m;
+    lv->data.tableVal = avm_table_new();
+    avm_table_inc_refcounter(lv->data.tableVal);
 }
 
 void execute_tablegetelem(instruction* instr){
