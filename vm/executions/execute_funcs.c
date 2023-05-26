d0612cbcbd1e18f03dc7a6fa50e3defacd120a7c
panoklis <csd3665@csd.uoc.gr>
2023-05-26 14:17:06 +0300

    feat(funccalls): execute_pusharg
    pushes each param in stack, for function executions

--- vm/executions/execute_funcs.c
+++ vm/executions/execute_funcs.c
@@ -1,12 +1,27 @@
 #include "execute.h"
 #include <stdio.h>
 
+extern avm_memcell ax, bx, cx;
+extern avm_memcell retval;
+extern unsigned top, topsp;
+extern unsigned pc;
+extern avm_memcell stack[AVM_STACKSIZE];
+extern unsigned char executionFinished;
+
+extern unsigned currLine;
+extern unsigned totalActuals;
+
 void execute_call(instruction* instr){
     printf("execute_call\n");
 }
 
 void execute_pusharg(instruction* instr){
-    printf("execute_pusharg\n");
+    avm_memcell *arg = avm_translate_operand(&instr->result, &ax);
+    assert(arg);
+
+    avm_assign(&stack[top], arg);
+    ++totalActuals;
+    avm_dec_top();
 }
 
 void execute_funcenter(instruction* instr){
