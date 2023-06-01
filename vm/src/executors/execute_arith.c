f9623dc5c804f11ddb552507ea723b2f32011847
Efthymis Papageorgiou <papageorgiou.efthymis@protonmail.com>
2023-06-01 20:27:42 +0300

    refactor(vm): exec_assign has it's own file

--- vm/src/executors/execute_arith.c
+++ vm/src/executors/execute_arith.c
@@ -61,43 +61,6 @@ arithmetic_func_t arithmeticFuncs[] = {
     mod_impl
 };
 
-void avm_assign(avm_memcell* lv, avm_memcell* rv){
-    
-    if(lv == rv)
-        return;
-
-    if(lv->type == table_m && 
-       rv->type == table_m && 
-       lv->data.tableVal == rv->data.tableVal)
-        return;
-    
-    if(rv->type == undef_m)
-        avm_warning("assigning from 'undef' content!", currLine);
-    
-    avm_memcell_clear(lv);
-
-    memcpy(lv, rv, sizeof(avm_memcell));
-
-    if(lv->type == string_m)
-        lv->data.strVal = strdup(rv->data.strVal);
-
-    else if(lv->type == table_m)
-        avm_table_inc_refcounter(lv->data.tableVal);
-    
-}
-
-void execute_assign(instruction* instr){
-    if(DEBUG) printf("execute_assign\n");
-
-    avm_memcell* lv = avm_translate_operand(instr->result, (avm_memcell*) 0);
-    avm_memcell* rv = avm_translate_operand(instr->arg1, &ax);
-
-    assert(lv && ((&stack[AVM_STACKSIZE - 1] >= lv && lv > &stack[top]) || lv == &retval));
-    assert(rv);
-
-    avm_assign(lv, rv);
-}
-
 void execute_arithmetic(instruction* instr){
     if(DEBUG) printf("execute_arithmetic\n");
 
